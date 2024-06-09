02d87e0be30607c7444a1c44c8c930a6b759187b
josue <josuejuniorww@gmail.com>
2024-06-08 23:52:47 -0300

    feat: Permission resource

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -4,8 +4,69 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Permission;
+use App\Http\Requests\PermissionRequest;
 
 class PermissionController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $permissions = Permission::all();
+        return view('admin.permission.index', ['permissions' => $permissions]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.permission.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PermissionRequest $request)
+    {
+        $permission = Permission::create($request->validated());
+
+        return redirect()->route('admin.permission.show', ['permission' => $permission]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Permission $permission)
+    {
+        return view('admin.permission.show', ['permission' => $permission]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Permission $permission)
+    {
+        return view('admin.permission.edit', ['permission' => $permission]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(PermissionRequest $request, Permission $permission)
+    {
+        $permission->update($request->validated());
+        return redirect()->route('admin.permission.show', ['permission' => $permission]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+        return redirect()->route('admin.permission.index');
+    }
 }
---
+++ resources/views/admin/permission/index.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="col-12">
+    <div class="card m-3">
+        <div class="card-header justify-content-between">
+            <h3 class="card-title">Lista de permissões</h3>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+            <div>
+                <a href="{{ route('admin.permission.create') }}" class="btn btn-success w-100 mb-1">
+                    Adicionar nova permissão
+                </a>
+            </div>
+        </div>
+      <div class="table-responsive m-4">
+        <table class="display w-100" id="tabela-permissions"> {{-- table card-table table-vcenter text-nowrap datatable --}}
+          <thead>
+            <tr>
+              {{--<th class="w-1"></th>  <input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices"> --}}
+              <th>Nome</th>
+              <th>Criado em</th>
+              <th>Atualizado em</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($permissions as $permission)
+            <tr>
+                <!--<td></td>  <input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"> -->
+                <td>{{ $permission->name }}</td>
+                <td>{{ \Carbon\Carbon::parse($permission->created_at)->format('d/m/Y G:h') }}</td>
+                <td>{{ \Carbon\Carbon::parse($permission->updated_at)->format('d/m/Y G:h') }}</td>
+                <td class="text-end">
+                    <a class="btn justify-content-center" href="{{ route('admin.permission.show', ['permission' => $permission]) }}">
+                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-eye"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
+                        Visualizar
+                    </a>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('js')
+<script>
+    $(document).ready( function () {
+        $('#tabela-permissions').DataTable({
+            "paging": true,
+            "ordering": true,
+            "searching": true,
+            "pageLength": 10,
+            "language": {
+                url: '//cdn.datatables.net/plug-ins/2.0.3/i18n/pt-BR.json',
+            },
+        });
+    });
+</script>
+@endsection
+
