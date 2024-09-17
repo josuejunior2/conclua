ed84f75e54177b82b69e2eb0604386e7a836443a
josue <josuejuniorww@gmail.com>
2024-09-17 12:01:43 -0300

    refactor: desativa exclui arquivo sub se tiver nota

--- resources/views/arquivo/download_submissao.blade.php
+++ resources/views/arquivo/download_submissao.blade.php
@@ -27,31 +27,33 @@
                                         </button>
                                     </form>
                                 </div>
-                                @can('excluir arquivo submissao')
-                                    @include('arquivo.destroy_arquivo_submissao')
-                                    <div class="col-auto">
-                                        <form id="form_destroy_arquivo_submissao_{{ $arquivo->id }}" method="post"
-                                            action="{{ route('arquivo.destroy.arquivo.submissao', ['arquivo' => $arquivo]) }}">
-                                            @method('DELETE')
-                                            @csrf
-                                            <a href="#" class="btn btn-outline-danger w-100" data-bs-toggle="modal"
-                                                data-bs-target="#modal-destroy-arquivo-submissao{{ $arquivo->id }}">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="icon icon-tabler icon-tabler-trash" width="24" height="24"
-                                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M4 7l16 0" />
-                                                    <path d="M10 11l0 6" />
-                                                    <path d="M14 11l0 6" />
-                                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                                                </svg>
-                                                Excluir
-                                            </a>
-                                        </form>
-                                    </div>
-                                @endcan
+                                @if(session('semestreIsAtivo') && empty($atividade->nota))
+                                    @can('excluir arquivo submissao')
+                                        @include('arquivo.destroy_arquivo_submissao')
+                                        <div class="col-auto">
+                                            <form id="form_destroy_arquivo_submissao_{{ $arquivo->id }}" method="post"
+                                                action="{{ route('arquivo.destroy.arquivo.submissao', ['arquivo' => $arquivo]) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <a href="#" class="btn btn-outline-danger w-100" data-bs-toggle="modal"
+                                                    data-bs-target="#modal-destroy-arquivo-submissao{{ $arquivo->id }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                        class="icon icon-tabler icon-tabler-trash" width="24" height="24"
+                                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M4 7l16 0" />
+                                                        <path d="M10 11l0 6" />
+                                                        <path d="M14 11l0 6" />
+                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                                    </svg>
+                                                    Excluir
+                                                </a>
+                                            </form>
+                                        </div>
+                                    @endcan
+                                @endif
                             </div>
                         </div>
                     </div>
